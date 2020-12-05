1dff0610be648a3170497985488fb02c30465068
ediko <edvard.gogidze.1@btu.edu.ge>
2020-12-05 06:52:09 -0800

    NEW

--- quizz/app/Http/Controllers/QuizController.php
+++ quizz/app/Http/Controllers/QuizController.php
@@ -12,6 +12,11 @@ use Illuminate\Http\Request;
 
 class QuizController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function create()
     {
         return view('admin.create');
